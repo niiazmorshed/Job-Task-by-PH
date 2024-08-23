440152ee8872f19223f6f72faac726bdea5d88bc
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-08-23 19:20:12 +0600

    Project Done

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const loadWeather = async (searchText) => {
     `https://api.weatherapi.com/v1/current.json?key=b4dded29c6f947c7aa9224318242108&q=${searchText}`
   );
   const data = await res.json();
+  console.log(data)
   displayWeather(data);
+  
 };
 
 const loadW = async (searchText) => {
@@ -19,16 +21,18 @@ const displayWeather = (i) => {
   const weatherContainer = document.getElementById("weatherContainer");
   weatherContainer.textContent = "";
   const weatherCard = document.createElement("div");
-  weatherCard.classList = `flex gap-4`;
+  weatherCard.classList = `flex gap-4 py-4`;
   weatherCard.innerHTML = `
   
-  <div><h1 class="text-8xl font-manrope">${i.current.temp_c}</h1></div>
+  <div><h1 class="text-8xl font-manrope">${i.current.temp_c} </h1>
+  </div>
+  <i class="fa-solid fa-circle-notch fa-lg"></i>
   <div>
-  <h3 class="text-4xl font-manrope" >${i.location.name}</h3>
-  <h3 class="text-2xl font-manrope" >${i.location.localtime}</h3>
+  <h3 class="text-6xl font-manrope font-bold text-blue-400" >  ${i.location.name}</h3>
+  <h3 class="text-2xl font-manrope font-extrabold text-yellow-600" ><span class="font-bold ">Date & Time:</span> ${i.location.localtime}</h3>
+  <h1 class="text-2xl font-manrope font-extrabold text-yellow-600" > <span class=" font-bold font-manrope text-2xl " >Weather Condition:</span> ${i.current.condition.text}</h1>
   </div>
   <div><img src="//cdn.weatherapi.com/weather/64x64/night/353.png" alt=""></div>
-  
   `;
   weatherContainer.appendChild(weatherCard);
 
@@ -39,35 +43,33 @@ const displayWeather = (i) => {
   forecastCard.innerHTML = `
     <div>
     
-    <h1 class="text-6xl text-center font-manrope">Weather Details</h1>
+    <h1 class="text-6xl text-center font-manrope font-bold py-4">Weather Details</h1>
     <br/>
-    <h2 class= "text-3xl text-center font-manrope" >Overcast Clouds</h2>
-
     <div class="flex justify-around font-manrope">
-        <p>Temp Max</p>
-        <p>${i.current.heatindex_c} <i class="fa-solid fa-temperature-three-quarters fa-xl" style="color: #e90101;"></i> </p>
+        <p class="text-2xl font-manrope font-bold">Temp Max</p>
+        <p class="text-2xl font-bold">${i.current.heatindex_c} <i class="fa-solid fa-temperature-three-quarters fa-xl" style="color: #e90101;"></i> </p>
         
     </div>
     <br />
 
     <div class="flex justify-around font-manrope">
-        <p>Temp Min</p>
-        <p>${i.current.dewpoint_c} <i class="fa-solid fa-temperature-three-quarters fa-xl" style="color: #74C0FC;"></i> </p>
+        <p class="text-2xl font-manrope font-bold" >Temp Min</p>
+        <p class="text-2xl font-bold">${i.current.dewpoint_c} <i class="fa-solid fa-temperature-three-quarters fa-xl" style="color: #74C0FC;"></i> </p>
     </div>
     <br />
     <div class="flex justify-around font-manrope">
-        <p>Humanidity</p>
-        <p class= "font-manrope" >${i.current.humidity}% <i class="fa-solid fa-water fa-xl" style="color: #74C0FC;"></i></p>
+        <p class="text-2xl font-manrope font-bold" >Humanidity</p>
+        <p class= "font-manrope text-2xl font-bold" >${i.current.humidity}% <i class="fa-solid fa-water fa-xl" style="color: #74C0FC;"></i></p>
     </div>
     <br />
         <div class="flex justify-around font-manrope">
-        <p>Cloudy</p>
-        <p>${i.current.cloud}% <i class="fa-solid fa-cloud fa-xl" style="color: #74C0FC;"></i></p>
+        <p class="text-2xl font-bold font-manrope" >Cloudy</p>
+        <p class="text-2xl font-bold" >${i.current.cloud}% <i class="fa-solid fa-cloud fa-xl" style="color: #74C0FC;"></i></p>
     </div>
     <br />
         <div class="flex justify-around font-manrope">
-        <p>Wind</p>
-        <p class="font-manrope">${i.current.wind_kph} km/h</p>
+        <p class="text-2xl font-manrope font-bold" >Wind</p>
+        <p class="font-manrope text-2xl font-bold">${i.current.wind_kph} km/h</p>
     </div>
     <br />
     </div>
